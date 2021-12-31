eae25061f52b8f859fc6a0da33f04ebbd61009d4
JyothiRegula <96280490+JyothiRegula@users.noreply.github.com>
2021-12-31 16:55:38 +0530

    ExtentReport

---
+++ src/main/java/Practice_Scripts/ExtentReport1.java
@@ -0,0 +1,90 @@
+package Practice_Scripts;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ExtentReport1 {
+
+		ExtentHtmlReporter htmlreporter;
+		ExtentReports extent;
+		WebDriver driver;
+
+		@BeforeSuite
+
+		public void setUp()
+		{
+			// start reporters
+			htmlreporter = new ExtentHtmlReporter("Lavanish2.html");
+
+			// create extent reports and attach reporter(s)
+			extent =new ExtentReports();
+			extent.attachReporter(htmlreporter);
+
+		}
+
+		@BeforeTest
+		public void setUpTest()
+		{
+
+			String projectPath=System.getProperty("user.dir");
+
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}
+
+		@Test
+		public void test1()
+		{
+
+			ExtentTest test = extent.createTest( Thread.currentThread().getStackTrace()[1].getMethodName(), "Sample Description");
+			driver.get("https://www.facebook.com/");
+
+			// log(LogStatus, details)
+			test.pass("Navigate to facebook");
+			test.log( Status.INFO,  "This step shows usage of log(status, details)");
+
+
+			// close the popup
+			// click on the login button
+
+		} 
+
+		@AfterTest
+		public void tearDownStep()
+		{
+			driver.close();
+			driver.quit();
+			System.out.println("Test completed successfully ");
+
+
+		}
+		@AfterSuite
+
+		public void tearDown()
+		{
+
+			// calling flush writes everything to the log file
+			driver.quit();
+			extent.flush(); 
+		}
+
+	}
+
+
+
+
--- src/main/java/Practice_Scripts/ExtentReportDemo.java
+++ src/main/java/Practice_Scripts/ExtentReportDemo.java
@@ -1,7 +1,5 @@
 package Practice_Scripts;
-
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,65 +17,52 @@ import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ExtentReportDemo {
-	
-	
-	public  WebDriver driver;
-	private ExtentHtmlReporter htmlreporter;
-	ExtentReports extent;
-	
-	
-	//@BeforeTest
-	
-	public void run1() throws Throwable{
-		// start reporters
-	 htmlreporter = new ExtentHtmlReporter("extentReports4.html");
 
-		// create extent reports and attach reporter(s)
+	
+
+ 
+	 	
+	 @Test	
+	 public void LavanishPatel()
+	 {
+	 ExtentHtmlReporter htmlreporter;
+	 ExtentReports extent;
+	 WebDriver driver;	 
+	 // start reporters
+	 htmlreporter = new ExtentHtmlReporter("Appy2.html");
+	 // create extent reports and attach reporter(s)
 	 extent =new ExtentReports();
-		extent.attachReporter(htmlreporter);
-		
-	}
-	@Test
-	public void run2() throws Throwable {
-		// creates a toggle for the given test, adds all log events under it    
-		 ExtentTest test1 = extent.createTest("oxhead search","this is a test to validate oxhead site login funtionality");
+	 extent.attachReporter(htmlreporter);
 
+	 
 		String projectPath=System.getProperty("user.dir");
-		System.getProperty("webdriver.chrome.driver",projectPath+"./drivers");
+
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	 
+		ExtentTest test = extent.createTest(Thread.currentThread().getStackTrace()[1].getMethodName(), "Sample Description");
+		driver.get("https://www.facebook.com/");
 
 		// log(LogStatus, details)
-		test1.log(Status.INFO,"Starting test case");
-		test1.pass("Navigate to oxhead site");
+		test.pass("Navigate to facebook");
+		test.log( Status.INFO,  "This step shows usage of log(status, details)");
+		System.out.println("Test completed successfully ");
 
-		driver.get("https://oxheaduatstg.wpengine.com/?v=47e5dceea252");
-		Thread.sleep(3000);
-		
+
 		// close the popup
-		driver.findElement(By.className("mfp-close")).click();
-		test1.pass("clicked on the login button");
-		Thread.sleep(2000);
-
 		// click on the login button
-		driver.findElement(By.className("wd-tools-text")).click();
-		Thread.sleep(2000);
-		
-		test1.pass("closed the browser");
-		test1.info("Test completed");
-	}
-		 
-		
-		//@AfterTest
+
+	 
+		driver.close();
+		driver.quit();
+		extent.flush();
+	    
+
 	
-		public void tearDown() {
-			 
-			// calling flush writes everything to the log file
-			driver.quit();;
-		}
-		
-	}
+	 }
+	 
+ }
 
 
 
