9e5071569d1a65a6092f86b099a4358d354ad01d
JyothiRegula <96280490+JyothiRegula@users.noreply.github.com>
2022-01-03 18:26:35 +0530

    listenerstestNG

--- src/main/java/Listeners/TestCase1.java
+++ src/main/java/Listeners/TestCase1.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class TestCase1 {
+public class TestCase1  {
 
 	@Test
 	public void googleTitle() {
